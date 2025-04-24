773670689e44b5c0f0fa341bf8fbf1451f3e2e43
Fakeman <55951275+fakemanoan@users.noreply.github.com>
2025-04-24 13:33:16 +0100

    Add WifiOverlay

--- device.mk
+++ device.mk
@@ -51,7 +51,8 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-	TetheringConfigOverlay
+	TetheringConfigOverlay \
+	WifiOverlay
 
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
